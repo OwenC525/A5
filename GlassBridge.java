94db95093cae38edd295c4c54bc736986e5c9fb1
Owen Chiu <owencc8@gmail.com>
2025-10-27 12:40:50 -0400

    first commit

---
+++ GlassBridge.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write the GlassBridge program inside the main method
+ * according to the assignment description.
+ *
+ * When executing your code, copy from the assignment detail and make sure it matches with what you should be getting.
+ * After reading the bridge details by column and storing it in a 2-D array, read the path by row and store that in a 1D array.
+ * Then loop through the bridge according to the path making sure the bridge is safe, True means the panel on the bridge is safe, False means the panel is unsafe.
+ * At the end, if the path leads you all the way to the end of the bridge without encountering a false block, return Safe, else return False.
+ *
+ * To compile:
+ *        javac GlassBridge.java
+ * To execute:
+ *        java GlassBridge *enter the glass bridge true/false* *enter the "LR" sequence (path) you want to test*
+ *
+ * DO NOT change the class name
+ * DO NOT use System.exit()
+ * DO NOT change add import statements
+ * DO NOT add project statement
+ *
+ *  @author Ved Patel
+ */
+public class GlassBridge {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
